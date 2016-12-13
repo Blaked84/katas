3fab0b90eba294d0b8d757d26f2dc7bb38968c09
blaked <dorian.becker@gadz.org>
2016-12-13 20:56:29 +0100

    kata complete

--- filedb/lib/file_db/database.rb
+++ filedb/lib/file_db/database.rb
@@ -1,6 +1,23 @@
+require 'json'
+require 'byebug'
+
 module FileDb
   class Database
+
+    attr_reader :data
+
     def initialize(data_file)
+
+      data_json = File.read(data_file)
+      @data = JSON.parse(data_json)
+    end
+
+    def table_names
+      @data.keys.reverse
+    end
+
+    def table(name)
+      FileDb::Table.find_by_name(self.data, name)
     end
   end
 end
--- filedb/lib/file_db/table.rb
+++ filedb/lib/file_db/table.rb
@@ -1,4 +1,58 @@
 module FileDb
   class Table
+    attr_reader :data, :name, :database
+
+    def initialize(data=nil, table_mane=nil, database_name=nil)
+      @data = data
+      @name = table_mane
+      @database = database_name
+    end
+
+    def self.find_by_name(db, name)
+      table = self.new(db.data[name], name)
+      return table
+    end
+
+
+    def select(args = nil)
+      if args.nil?
+        # if no parameter, return full data collection
+        self.data
+      else
+        where_condition = args[:where]
+        key = where_condition.keys.first.to_s
+        value_to_match = where_condition.values.first
+        self.data.select{ |x| x[key] == value_to_match }
+      end
+    end
+
+    def insert(args = nil)
+      if args.nil?
+        raise "You can't insert nothing dude !"
+      elsif !args.is_a?(Hash)
+        raise "You can only instert an Hash"
+      else
+        #get last id
+        last_id = self.data.collect{|x| x["id"]}.compact.max
+        # generate hash with id
+        hash_to_insert = {"id" => last_id+1}
+        hash_to_insert.merge!(args)
+        self.data << hash_to_insert
+      end
+    end
+
+    def update(args)
+      where_condition = args[:where]
+      values = args[:values]
+      value_key = values.keys.first.to_s
+      value_value = values.values.first
+
+      current_hashs = self.select(where: where_condition)
+      # update hash
+      current_hashs.each do |c_h|
+        c_h[value_key] = value_value
+      end
+    end
+
   end
 end
